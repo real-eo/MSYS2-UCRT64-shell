ac9ff7949b0a71af6e980798eabab6070e7f4547
Elias Alexander Wiklund Ottersbo <realeawo5@gmail.com>
2026-09-09 00:50:13 +0200

    fix: added MUI verb and dll.cpp impl

--- src/dll.cpp
+++ src/dll.cpp
@@ -3,8 +3,9 @@
 #include "ClassFactory.h"
 #include "Guids.h"
 
-extern "C" __declspec(dllexport) 
-HRESULT STDAPICALLTYPE DllGetClassObject(REFCLSID rclsid, REFIID riid, void** object) {
+// ? Since the linker pragmas preform the exports, we don't need to use `__declspec(dllexport)` here. 
+extern "C" // __declspec(dllexport)
+HRESULT STDAPICALLTYPE DllGetClassObjectImpl(REFCLSID rclsid, REFIID riid, void** object) {
     if (object == nullptr)
         return E_POINTER;
 
@@ -21,8 +22,35 @@ HRESULT STDAPICALLTYPE DllGetClassObject(REFCLSID rclsid, REFIID riid, void** ob
     return result;
 }
 
-extern "C" __declspec(dllexport) 
-HRESULT STDAPICALLTYPE DllCanUnloadNow() {
+// ? Since the linker pragmas preform the exports, we don't need to use `__declspec(dllexport)` here. 
+extern "C" // __declspec(dllexport) 
+HRESULT STDAPICALLTYPE DllCanUnloadNowImpl() {
     // ? Returning `S_FALSE` is fine temporarily. Later, it will be replaced with a real global object/server-lock count.
     return S_FALSE;
 }
+
+
+
+// * NOTE: The following two lines are required to avoid the `C2375` error.
+/* // ! ERROR:
+    === Building [Debug] ===
+    MSBuild version 18.8.2+ce25c0108 for .NET Framework
+
+      dll.cpp
+    C:\Users\reale\Code\C++\MSYS2-UCRT-shell\src\dll.cpp(7,24): error C2375: 'DllGetClassObject': redefinition; different linkage [C:\Users\reale\Code\C++\MSYS2-UCRT-shell\build\msys2_ucrt64_shell.vcxproj]
+          C:\Program Files (x86)\Windows Kits\10\Include\10.0.26100.0\um\combaseapi.h(1331,9):
+          see declaration of 'DllGetClassObject'
+
+    C:\Users\reale\Code\C++\MSYS2-UCRT-shell\src\dll.cpp(25,24): error C2375: 'DllCanUnloadNow': redefinition; different linkage [C:\Users\reale\Code\C++\MSYS2-UCRT-shell\build\msys2_ucrt64_shell.vcxproj]
+          C:\Program Files (x86)\Windows Kits\10\Include\10.0.26100.0\um\combaseapi.h(1334,9):
+          see declaration of 'DllCanUnloadNow'
+*/
+
+//  The error occurs because the `DllGetClassObject` and `DllCanUnloadNow` functions are already declared in `combaseapi.h`. 
+//  The linker will complain about the redefinition of these functions if they are defined in this file. To avoid this, we 
+//  use the `/export` linker option to export our own implementations of these functions with different names (`DllGetClassObjectImpl` 
+//  and `DllCanUnloadNowImpl`). This way, we can provide our own implementations without conflicting with the existing declarations.
+//  We additionally use the `PRIVATE` option to suppress LNK4104 warnings about duplicate exports. 
+
+#pragma comment(linker, "/export:DllGetClassObject=DllGetClassObjectImpl,PRIVATE")
+#pragma comment(linker, "/export:DllCanUnloadNow=DllCanUnloadNowImpl,PRIVATE")

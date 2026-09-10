a6bc6ac2bf16bc7df55b93c6c91620c40bb6ae66
Elias Alexander Wiklund Ottersbo <realeawo5@gmail.com>
2026-09-10 04:31:51 +0200

    feat: add stub executable and update AppxManifest for new executable name; replace build script with pack script

--- scripts/pack-msix.ps1
+++ scripts/pack-msix.ps1
@@ -1,2 +1,2 @@
-# Build the MSIX package
+# Pack (manifest + assets only - no DLL)
 & "C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\makeappx.exe" pack /d .\packaging /p .\packaging\Msys2Ucrt64Shell.msix /o
